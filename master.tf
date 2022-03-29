ab46a5aa092d7fb3923ffbdd80e7fedb738f8f20
marcoslobo <marcos.lobo.nit@gmail.com>
2022-03-29 14:14:09 -0300

    slaves and files

--- master.tf
+++ master.tf
@@ -21,6 +21,14 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# create public IPs
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.prefix}-tfip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+}
+
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   resource_group_name = azurerm_resource_group.main.name
@@ -30,33 +38,77 @@ resource "azurerm_network_interface" "main" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  size                            = "Standard_F2"
-  admin_username                  = "adminuser"
+  name                = "${var.prefix}-master"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_F2"
+  admin_username      = "ubuntu"
   network_interface_ids = [
     azurerm_network_interface.main.id,
   ]
 
   admin_ssh_key {
-    username = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    username   = "ubuntu"
+    public_key = file("${var.master_ssh_public_key_file}")
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal-daily"
+    sku       = "20_04-daily-lts"
     version   = "latest"
   }
 
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
+  }
+
+
+  connection {
+    host        = azurerm_public_ip.pip.ip_address
+    user        = "ubuntu"
+    type        = "ssh"
+    private_key = file("${var.master_ssh_private_key_file}")
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo ufw disable",
+      "sudo apt-get update",
+      "sudo mkdir /jmeter-master",
+      "sudo chown -R ubuntu /jmeter-master",
+      "sudo apt install apt-transport-https ca-certificates curl software-properties-common -y",
+      "sudo mkdir /opt/jdk",
+      "sudo chown -R ubuntu /opt/jdk",
+      "cd /opt/jdk",
+      "curl https://jmeter-multicloud.s3.amazonaws.com/jdk-8u202-linux-x64.tar.gz> java8.tgz",
+      "sudo tar -zxf java8.tgz",
+      "sudo update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_202/bin/java 100",
+      "sudo curl -L https://github.com/docker/compose/releases/download/v2.3.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+      "sudo mkdir /usr/local/bin/docker-compose",
+      "sudo chown -R ubuntu /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable'",
+      "sudo apt install docker-ce -y",
+    ]
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/files"
+    destination = "/jmeter-master"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker-compose -f ~/../../jmeter-master/files/docker-compose.yml up -d"
+    ]
   }
 }
